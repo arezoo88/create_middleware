2b3000ed90dd339e48bfc0bb74d35c6e4fdf3ef3
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-17 22:06:44 +0430

    add person model and calculate method in admin.py

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Person
 
-# Register your models here.
+
+@admin.register((Person))
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'is_adult')
+
+    def is_adult(self, obj):
+        if obj.age > 18:
+            return True
+        else:
+            return False
+
+    is_adult.boolean = True
