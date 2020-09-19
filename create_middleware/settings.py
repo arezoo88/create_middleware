fab11c9dc921584d7e5970ffeec414211dc0dc2e
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-19 11:50:32 +0430

    add SearchVector and TrigramSimilarity for search in fields

--- create_middleware/settings.py
+++ create_middleware/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -27,7 +26,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'django.contrib.postgres' #full text search work iin postgres
 ]
 
 MIDDLEWARE = [
@@ -71,17 +70,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'create_middleware.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'textsearch',
+        'USER': 'postgres',
+        'PASSWORD': '8812621012',
+        'HOST': 'localhost'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -101,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -115,13 +120,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-#message
+# message
 from django.contrib.messages import constants as messages
+
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
--- home/views.py
+++ home/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import authenticate, logout, login
 from .models import Person
 from django.db.models import Q
 from .forms import SearchForm
+from django.db.models.functions import Greatest
+# use search vector
+from django.contrib.postgres.search import SearchVector, TrigramSimilarity
 
 
 def index(request):
@@ -13,7 +16,11 @@ def index(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             cd = form.cleaned_data['search']
-            persons = persons.filter(Q(name__icontains=cd) | Q(description__icontains=cd))
+            # persons = persons.filter(Q(name__icontains=cd) | Q(description__icontains=cd))
+            # persons = persons.annotate(search = SearchVector('name','description')).filter(search=cd) #daghighan hamun kalameha
+            # persons = persons.annotate(similarity=TrigramSimilarity('name', cd), ).filter(similarity__gt=0.3) \
+            #     .order_by('-similarity')
+            persons = persons.annotate(similarity=Greatest(TrigramSimilarity('name', cd), TrigramSimilarity('description', cd))).filter(similarity__gte=0.1).order_by('-similarity')
     return render(request, 'home/index.html', {'persons': persons, 'form': form})
 
 
